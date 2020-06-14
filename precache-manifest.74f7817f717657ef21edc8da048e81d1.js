4987ce97b4a3d32d517fd05700253d78a4ac003b
Nick Doiron <ndoiron@mapmeld.com>
2020-06-14 14:24:23 -0400

    static site

---
+++ precache-manifest.74f7817f717657ef21edc8da048e81d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec206d06633dbdf7264b264ea7f8e1df",
+    "url": "/uni-calculator/index.html"
+  },
+  {
+    "revision": "02e16e0d49a1b48dd3e7",
+    "url": "/uni-calculator/static/css/main.25af0fd3.chunk.css"
+  },
+  {
+    "revision": "5788b4a77c1aa260c214",
+    "url": "/uni-calculator/static/js/2.968e3c08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uni-calculator/static/js/2.968e3c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e16e0d49a1b48dd3e7",
+    "url": "/uni-calculator/static/js/main.30883283.chunk.js"
+  },
+  {
+    "revision": "dace2951fdc5d6b1979f",
+    "url": "/uni-calculator/static/js/runtime-main.7f1766f1.js"
+  }
+]);
