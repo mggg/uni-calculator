ffdb5d9e16c6c0276081560176a192eee70e8105
Parker J. Rule <parker.rule@tufts.edu>
2020-06-14 23:58:19 -0400

    Minor formatting changes

--- src/App.js
+++ src/App.js
@@ -158,21 +158,20 @@ class App extends React.Component {
           </nav>
 
           <section className="qSection">
-            <h3>Description</h3>
-            <p style={{textAlign: 'left'}}>This is a scenario-building tool to help university leadership arrive at plans for COVID testing cohorts and
-            frequency for the Fall 2020 semester.  Its intention is to help universities plan a bulk testing commitment in June,
+            <p style={{textAlign: 'left', padding: '10px'}}><strong>This is a scenario-building tool to help university leadership arrive at plans for COVID testing cohorts and
+            frequency for the Fall 2020 semester.</strong>  Its intention is to help universities plan a bulk testing commitment in June,
             such as (but not exclusively) the testing service to be offered by the Broad Institute.</p>
           </section>
 
           <section className="qSection suggestions">
-            <h3>Summary of modeling recommendations</h3>
-            <p>Quick review of suggested frequency for surveillance testing with isolation strategy, without assumptions on contact tracing.</p>
+            <h4>Summary of modeling recommendations</h4>
+            <p>Here&rsquo;s a quick review of suggested frequency for surveillance testing with isolation strategy, without assumptions on contact tracing.</p>
             <ul>
               <li>Rochelle Walensky’s team (Harvard Med) recommends Q3 (that is, every three days) testing in order to keep the total number of infections minimized while keeping costs in reasonable range.</li>
               <li>Peter Frazier’s group (Cornell operations research) recommends Q5 testing but does not consider exogenous shocks, may increase frequency when that parameter is added.</li>
               <li>The IDSS COVID Collaboration (contact: Peko Hosoi at MIT) finds that Q12 testing suffices for stability (i.e., to prevent exponential outbreak within campus) with R0 = 2 and 100% sensitivity, but this finding does not take into account total number of infections, only rate of growth. By contrast, R0 = 2.5 and 70% sensitivity leads to recommendation of Q5.6, still only taking growth rate into account. The group’s survey of SIR/MCMC/network models indicates that Q12 testing could result in over half of campus infected, while Q3 is likely under 1%.</li>
             </ul>
-            <p><strong>Bottom line:</strong> a strategy where every individual is tested twice weekly would be in line with all available credible models. A weekly strategy is still extremely helpful for maintaining a steady and tolerably low infection rate on campus.</p>
+            <p><strong>The bottom line:</strong> a strategy where every individual is tested twice weekly would be in line with all available credible models. A weekly strategy is still extremely helpful for maintaining a steady and tolerably low infection rate on campus.</p>
           </section>
         </div>
 
@@ -185,7 +184,7 @@ class App extends React.Component {
           <small>One university may include multiple campuses</small>
           <br/>
           <br/>
-          Pre-fill data&nbsp;&nbsp;
+          University &nbsp;&nbsp;
           <select onChange={e => this.prefill(e.target.value)}>
             {this.state.colleges.map((c, i) => {
               return <option key={i} value={i}>
